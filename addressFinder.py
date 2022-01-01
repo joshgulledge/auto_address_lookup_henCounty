f651416423373ac8bb250d5847406a140b51bc42
joshgulledge <joshcalebgulledge@gmail.com>
2022-01-01 01:29:22 -0600

    fixed variable name bugs, trying to print owner name to console

--- addressFinder.py
+++ addressFinder.py
@@ -6,12 +6,17 @@ from selenium.webdriver.common.by import By
 
 # starts the browser, goes to webpage
 browser = webdriver.Firefox()
-browser2.get('https://gis.hennepin.us/property/map/default.aspx')
+browser.get('https://gis.hennepin.us/property/map/default.aspx')
 
 # gets the elements
-map_input = browser2.find_element(By.ID, "search-input")
-map_search_btn = browser2.find_element(By.ID, "search-btn")
+map_input = browser.find_element(By.ID, "search-input")
+map_search_btn = browser.find_element(By.ID, "search-btn")
 
 # manipulates the elements
 map_input.send_keys("9726 Utica Road, Bloomington") # input whatever address
-map_button.click()
+map_search_btn .click()
+
+#owner_name = browser.find_element()
+
+# quit the browser
+#browser.quit()

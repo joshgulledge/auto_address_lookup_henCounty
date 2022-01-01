b4050953bac3dbcabffb75e166390d592df98ae1
joshgulledge <joshcalebgulledge@gmail.com>
2022-01-01 02:23:07 -0600

    prints owner of address to console and closes browser

--- addressFinder.py
+++ addressFinder.py
@@ -3,6 +3,9 @@
 # imports from selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # starts the browser, goes to webpage
 browser = webdriver.Firefox()
@@ -16,7 +19,15 @@ map_search_btn = browser.find_element(By.ID, "search-btn")
 map_input.send_keys("9726 Utica Road, Bloomington") # input whatever address
 map_search_btn .click()
 
-#owner_name = browser.find_element()
+try:
+    element_present = EC.presence_of_element_located((By.XPATH, "//td[@id='owner']"))
+    WebDriverWait(browser, 10).until(element_present)
+except TimeoutException:
+    print("Timed out, waiting for page to load")
+finally:
+    # print owner of house to console
+    owner_name = browser.find_element(By.XPATH, "//td[@id='owner']")
+    print(owner_name.text)
 
 # quit the browser
-#browser.quit()
+browser.quit()

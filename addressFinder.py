a2b83f71aaf2f7c494e8b8995e35f4e083a5af6d
joshgulledge <joshcalebgulledge@gmail.com>
2022-01-01 20:55:26 -0600

    working on if page has a pop up for the condo units

--- addressFinder.py
+++ addressFinder.py
@@ -16,18 +16,27 @@ map_input = browser.find_element(By.ID, "search-input")
 map_search_btn = browser.find_element(By.ID, "search-btn")
 
 # manipulates the elements
-map_input.send_keys("9726 Utica Road, Bloomington") # input whatever address
+map_input.send_keys("8161 33rd Avenue South, Bloomington") # input whatever address
 map_search_btn .click()
 
 try:
-    element_present = EC.presence_of_element_located((By.XPATH, "//td[@id='owner']"))
-    WebDriverWait(browser, 10).until(element_present)
+    pop_up_present = EC.presence_of_element_located((By.XPATH, "//text()='503'"))
+    WebDriverWait(browser, 10).until(pop_up_present)
 except TimeoutException:
-    print("Timed out, waiting for page to load")
+    print("pop up not on page")
 finally:
-    # print owner of house to console
-    owner_name = browser.find_element(By.XPATH, "//td[@id='owner']")
-    print(owner_name.text)
+    # click the address with the condo number
+    print("pop up on page")
 
-# quit the browser
+# try:
+#     element_present = EC.presence_of_element_located((By.XPATH, "//td[@id='owner']"))
+#     WebDriverWait(browser, 10).until(element_present)
+# except TimeoutException:
+#     print("Timed out, waiting for page to load")
+# finally:
+#     # print owner of house to console
+#     owner_name = browser.find_element(By.XPATH, "//td[@id='owner']")
+#     print(owner_name.text)
+
+# # quit the browser
 browser.quit()

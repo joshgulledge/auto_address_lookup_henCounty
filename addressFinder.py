e5e377984fa67083f20fd132a3e4b98bfb2d33d3
joshgulledge <joshcalebgulledge@gmail.com>
2022-01-03 21:26:02 -0600

    automation works with condo number pop up now

--- addressFinder.py
+++ addressFinder.py
@@ -19,24 +19,27 @@ map_search_btn = browser.find_element(By.ID, "search-btn")
 map_input.send_keys("8161 33rd Avenue South, Bloomington") # input whatever address
 map_search_btn .click()
 
+condo_number = 503
+
 try:
-    pop_up_present = EC.presence_of_element_located((By.XPATH, "//text()='503'"))
+    pop_up_present = EC.presence_of_element_located((By.XPATH, "//td[text()='{condo_number}']"))
     WebDriverWait(browser, 10).until(pop_up_present)
 except TimeoutException:
     print("pop up not on page")
 finally:
     # click the address with the condo number
-    print("pop up on page")
+    condo_input_button = browser.find_element(By.XPATH, "//td[text()='503']/../../tr/td/input")
+    condo_input_button.click()
 
-# try:
-#     element_present = EC.presence_of_element_located((By.XPATH, "//td[@id='owner']"))
-#     WebDriverWait(browser, 10).until(element_present)
-# except TimeoutException:
-#     print("Timed out, waiting for page to load")
-# finally:
-#     # print owner of house to console
-#     owner_name = browser.find_element(By.XPATH, "//td[@id='owner']")
-#     print(owner_name.text)
+try:
+    element_present = EC.presence_of_element_located((By.XPATH, "//td[@id='owner']"))
+    WebDriverWait(browser, 10).until(element_present)
+except TimeoutException:
+    print("Timed out, waiting for page to load")
+finally:
+    # print owner of house to console
+    owner_name = browser.find_element(By.XPATH, "//td[@id='owner']")
+    print(owner_name.text)
 
 # # quit the browser
 browser.quit()
